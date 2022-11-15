841c4556dcbbe6db2bd773fd18965acc7e4555ed
pavelskiba <pavel.v.skiba@gmail.com>
2022-11-15 20:19:37 +0300

    feat: 04 search position hm added

---
+++ 04_search_sort/array_insertion/search_position/search_position.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int arr[] = {1,2,3,4,5,6,7,8,9};
+
+int size = 9;
+
+int x,p;
+
+cout << "Enter the number ";
+
+cin >> x;
+
+int k = search_position(arr, size, x);
+
+cout << k << endl << endl;
+
+system("pause 0");
+
+return 0;
+
+}

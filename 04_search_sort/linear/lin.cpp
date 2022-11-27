a7daadb6355a6ce6d20ff4f5a4cdaebaa304f091
pavelskiba <pavel.v.skiba@gmail.com>
2022-11-27 12:48:11 +0300

    fix

--- 04_search_sort/linear/lin.cpp
+++ 04_search_sort/linear/lin.cpp
@@ -16,13 +16,13 @@ int search(int arr[], int n, int x, int result[])
 int main()
 {
     int m[11] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19}; 
-    const int arrsize = sizeof(m) / sizeof(m[0]); // находим длину массива
-    int result[arrsize]; // создаём массив result и заполняем его нулями
+    const int arrsize = sizeof(m) / sizeof(m[0]); 
+    int result[arrsize]; 
     for (int i = 0; i < arrsize; ++i)
     {
         result[i] = 0;
     }
-    cout << "Count: " << search(m, arrsize, 19, result) << endl << "id : " << endl; // выводим количество найденных элементов и их номера
+    cout << "Count: " << search(m, arrsize, 19, result) << endl << "id : " << endl; 
     for (int i = 0; i < arrsize; ++i)
     {
         cout << result[i] << endl;
